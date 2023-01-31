03cfadaf6de457534afc7dae56d12c5af78603c3
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-01-31 16:49:17 +0200

    saved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+inputEl.addEventListener('input', debounce(getInputValue, DEBOUNCE_DELAY));
+
+function getInputValue(evt) {
+  console.log(evt.target.value);
+}
