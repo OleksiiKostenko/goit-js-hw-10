ce6b76162eb919b4ba792a0e84bd9291b14da186
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-02-01 17:28:51 +0200

    some works, some not

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,26 @@
+const listEl = document.querySelector('.country-list');
+const wrappEl = document.querySelector('.country-info');
+
 // fetchCountries(name);
+
+const url = fetch('https://restcountries.com/v3.1/name/peru')
+  .then(response => {
+    console.log(response);
+    return response.json();
+  })
+  .then(countrys => {
+    console.log(countrys[0]);
+    const country = countrys[0];
+    const countriesInfo = `<img src="${country.flags.svg}"   width="100"
+     height="50" alt="${country.flags.alt}" /><h1>${country.name.official}</h1>`;
+    const countriesList = ` <li><h2>Capital:</h2><p>${country.capital}</p></li>
+        <li><h2>Population:</h2><p>${country.population}</p></li>
+        <li><h2>Languages:</h2><p>${Object.values(country.languages)}</p></li>`;
+    wrappEl.insertAdjacentHTML('beforeend', countriesInfo);
+    listEl.insertAdjacentHTML('beforeend', countriesList);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -5,8 +5,20 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
+const listEl = document.querySelector('.country-list');
+const wrappEl = document.querySelector('.country-info');
 inputEl.addEventListener('input', debounce(getInputValue, DEBOUNCE_DELAY));
 
 function getInputValue(evt) {
-  console.log(evt.target.value);
+  const searchData = evt.target.value;
+  console.log(searchData);
 }
+
+// function createMarkup() {
+//   const countriesList = ` <li><h2>Capital:</h2><p>${capital}</p></li>
+//     <li><h2>Population:</h2><p>${population}</p></li>
+//     <li><h2>Languages:</h2><p>${languages}</p></li>`;
+//   const countriesInfo = `<img src="${flags.svg}" alt="${flags.alt}" /><h1>${name.official}</h1>`;
+//   listEl.insertAdjacentHTML('beforeend', countriesList);
+//   wrappEl.insertAdjacentHTML('beforeend', countriesInfo);
+// }
